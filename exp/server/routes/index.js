a75cfb3d620c41cb2ff83dd6aed800c580106480
yatyrio <abhinavk99@gmail.com>
2019-02-16 17:56:02 -0800

    Added keyword

--- exp/server/routes/index.js
+++ exp/server/routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var ke = require('keyword-extractor');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/keyword', js, function (req, res, next) {
+    var text = req.body.text;
+    var out = ke.extract(text, {
+        language: "english",
+        remove_digits: true,
+        return_changed_case: true,
+        remove_duplicates: true,
+    })
+    res.status(200).json({
+        'out': out
+    });
+})
 
 module.exports = router;
